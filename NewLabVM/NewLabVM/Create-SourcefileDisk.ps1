b4267e69f9a062f8d2afcf12a85fa27b6418c6e7
Jeremias Jansson <meklubba52@hotmail.com>
2015-11-17 12:56:46 +0100

    added write-verbose logging and ps remoting to set disk online and disable readonly

--- NewLabVM/NewLabVM/Create-SourcefileDisk.ps1
+++ NewLabVM/NewLabVM/Create-SourcefileDisk.ps1
@@ -2,52 +2,76 @@ Workflow Create-SourcefileDisk {
 	param(
 		[Parameter(Mandatory=$True)]
 		[string] $VMName,
+		[Parameter(Mandatory=$true)]
+		[PSCredential] $VMCredential,
 		[parameter(mandatory=$True)]
 		[string]$SourcefilesDir,
 		[parameter(mandatory=$True)]
 		[int]$DiskSizeInGB
 	)
-	function CreateVHD ($VHDPath, $Size) {
-		$drive = (New-VHD -path $VHDPath -SizeBytes $Size -Dynamic .\1033| `
-        Mount-VHD -Passthru | `
-        Get-Disk -number {$_.DiskNumber} | `
-        Initialize-Disk -PartitionStyle MBR -PassThru | `
-        New-Partition -UseMaximumSize -AssignDriveLetter:$False -MbrType IFS | `
-        Format-Volume -Confirm:$false -FileSystem NTFS -force | `
-        Get-Partition | `
-        Add-PartitionAccessPath -AssignDriveLetter -PassThru | `
-        get-volume).DriveLetter
+	Inlinescript {
+		$VMName = $using:VMName
+		$SourcefilesDir = $using:SourcefilesDir
+		$DiskSizeInGB = $using:DiskSizeInGB
+		$DiskSizeInBytes = $DiskSizeInGB *1024*1024*1024
 
-		Dismount-VHD $VHDPath
-	}
-	function MountVHD ($VHDPath) {
-		Mount-VHD $VHDPath
-		$drive = (Get-DiskImage -ImagePath $VHDPath | `
-        Get-Disk | `
-        Get-Partition).DriveLetter
-		"$($drive):\"
-		Get-PSDrive | Out-Null # Work around. some times the drive is not mounted
-	}
+		# VHDfunctions from http://www.padisetty.com/2013/11/creating-data-vhd-using-powershell.html
+		function CreateVHD ($VHDPath, $Size) {
+			$Drive = (New-VHD -path $VHDPath -SizeBytes $Size -Dynamic | `
+			Mount-VHD -Passthru | `
+			Get-Disk -number {$_.DiskNumber} | `
+			Initialize-Disk -PartitionStyle MBR -PassThru | `
+			New-Partition -UseMaximumSize -AssignDriveLetter:$false -MbrType IFS | `
+			Format-Volume -Confirm:$false -FileSystem NTFS -force | `
+			Get-Partition | `
+			Add-PartitionAccessPath -AssignDriveLetter -PassThru | `
+			get-volume).DriveLetter
+
+			Dismount-VHD $VHDPath
+		}
+		function MountVHD ($VHDPath) {
+			Mount-VHD $VHDPath
+			$Drive = (Get-DiskImage -ImagePath $VHDPath | `
+			Get-Disk | `
+			Get-Partition).DriveLetter
+			"$($Drive):\"
+			Get-PSDrive | Out-Null # Work around. some times the drive is not mounted
+		}
   
-	function DismountVHD ($VHDPath) {
-		Dismount-VHD $VHDPath
+		function DismountVHD ($VHDPath) {
+			Dismount-VHD $VHDPath
+		}
+	
+		# Main
+		# Get path to VM harddrives
+		Write-Verbose "Getting path to $VMName harddrives"
+		$VM = Get-VM -Name $VMName
+		$VMDiskLocation = $VM.HardDrives.path | Split-Path
+		Write-Verbose "Path to $VMName VHDdisk is $VMDiskLocation"
+		# Create a VHDX file
+		Write-Verbose "Creating $VMDiskLocation\$VMName-DataDisk.vhdx"
+		CreateVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx" -Size $DiskSizeInBytes
+ 
+		# Mount the VHD, copy data to it and finally dismount it
+		Write-Verbose "Mounting VHDx and copy data from $SourcefilesDir to it"
+		$VHDXPath = MountVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx"
+ 
+		# This will create a timestamp like yyyy-mm-yy
+		$TimeStamp = Get-Date -uformat "%Y-%m%-%d"
+		$RoboCopyParams = @("$SourcefilesDir","$VHDXPath","/MIR","/LOG+:$env:SystemRoot\Temp\SourcefilesRobocopy-$TimeStamp.log","/R:3","/W:30")
+		Write-Verbose "Copying sourcefiles to VHDx with robocopy params $RoboCopyParams"
+		& Robocopy $RoboCopyParams
+		Write-Verbose "Robocopy operation exitcode is $LASTEXITCODE, more info can be found in logfile $env:SystemRoot\Temp\SourcefilesRobocopy-$TimeStamp.log"
+ 
+		DismountVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx"
+
+		#Attach Disk to vm
+		Add-VMHardDiskDrive -VM $VM -Path "$VMDiskLocation\$VMName-DataDisk.vhdx" -ControllerType SCSI
 	}
 	
-	# Main
-	$VM = Get-VM -Name $VMName
-	$VMLocation = $VM.Path
-	$VMDiskLocation = $VM.HardDrives.path | Split-Path
-	# Create a VHDX file c:\temp\x.vhdx
-	CreateVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx" -Size $DiskSizeInGB`GB
- 
-	# Mount the VHD, copy c:\data to it and finally dismount it.
-	$VHDXPath = MountVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx"
- 
-	Copy $SourcefilesDir $VHDXPath -Recurse
- 
-	DismountVHD -VHDPath "$VMDiskLocation\$VMName-DataDisk.vhdx"
-
-	#Attach Disk
-	Add-VMHardDiskDrive -VM $VM -Path "$VMDiskLocation\$VMName-DataDisk.vhdx" -ControllerType SCSI
-
+	Inlinescript {
+		Write-Verbose "Remotely connected to $env:COMPUTERNAME"
+		Write-Verbose "Setting disk in online mode and disables readonly"
+		Get-Disk | Where {$_.OperationalStatus -eq 'offline'} | Set-Disk -IsOffline $false -IsReadOnly $false
+	} -PSComputerName $VMName -PSCredential $VMCredential
 }
