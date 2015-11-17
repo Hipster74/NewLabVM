c8abb1654cbb17fd9c1f79f69419e606b48affcd
Jeremias Jansson <meklubba52@hotmail.com>
2015-11-17 14:53:09 +0100

    Added Install-MDT2013U1 to repo

---
+++ NewLabVM/NewLabVM/Install-MDT2013U1.ps1
@@ -0,0 +1,41 @@
+workflow Install-MDT2013U1
+{
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$VMName,
+		[Parameter(Mandatory=$true)]
+		[PSCredential] $VMCredential,
+        [Parameter(Mandatory=$true,HelpMessage="Default is datadisk on d:\Source")]
+        [string]$SourceFilesParentDir
+    )    
+    
+    inlinescript {
+        $SourceFilesParentDir = $using:SourceFilesParentDir
+                        
+        try {
+            Write-Verbose "Starting MDT 2013 Update 1 installation"
+            if (Test-Path "$SourceFilesParentDir\MDT\MicrosoftDeploymentToolkit2013_x64.msi") {
+                $MDT2013U1UnattendArg = @("/qn","/L*v","$env:SystemRoot\Temp\MDT2013U1Install.log","REBOOT=ReallySuppress")
+                # Call MicrosoftDeploymentToolkit2013_x64.msi with arguments for unattended installation
+                & "$SourceFilesParentDir\MDT\MicrosoftDeploymentToolkit2013_x64.msi" $MDT2013U1UnattendArg
+                # Verify MDT 2013 Update 1 installation
+                if (($LASTEXITCODE = '0') -or ($LASTEXITCODE = '3010')) {
+                    Write-Output "MDT 2013 Update 1 installed successfully"
+                }
+                else {
+                    Write-Error "MDT 2013 Update 1 installation failed"
+                    Throw "MDT 2013 Update 1 installation failed"
+                }
+            }
+            else {
+                Write-Error "Could not find $SourceFilesParentDir\MDT\MicrosoftDeploymentToolkit2013_x64.msi , unable to install MDT 2013 Update 1"
+                Throw "Unable to locate MicrosoftDeploymentToolkit2013_x64.msi"
+            }
+        }
+        catch {
+            Write-Verbose "Failed to install MDT 2013 Update 1"
+            Write-Error $_.Exception
+        }
+    } -PSComputerName $VMName -PSCredential $VMCredential
+}
