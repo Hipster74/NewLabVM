31375b7083bd44e4aafda74cd3532ae1f8b57166
Jeremias Jansson <meklubba52@hotmail.com>
2015-11-19 16:44:51 +0100

    Added Throw if github xml failed to download, added -erroraction silentlycontinue on invoke-webrequest

--- NewLabVM/NewLabVM/Configure-LabADSrv.ps1
+++ NewLabVM/NewLabVM/Configure-LabADSrv.ps1
@@ -9,6 +9,7 @@ param(
 )
 
 Inlinescript{ 
+	$SrvSettingsXMLFile = $using:SrvSettingsXMLFile
 	Function Logit{
 		$TextBlock1 = $args[0]
 		$TextBlock2 = $args[1]
@@ -43,24 +44,24 @@ Inlinescript{
 	
 	# Get settingsfile from github
 	$Section = "Get XMLFile"
-	if (Test-Path "$env:SystemRoot\Temp\ad_srv_settings.xml") {
-		Logit "Found $env:SystemRoot\Temp\ad_srv_settings.xml , deleting it before downloading latest version"
-		Remove-Item "$env:SystemRoot\Temp\ad_srv_settings.xml"
+	if (Test-Path "$env:SystemRoot\Temp\$SrvSettingsXMLFile") {
+		Logit "Found $env:SystemRoot\Temp\$SrvSettingsXMLFile , deleting it before downloading latest version"
+		Remove-Item "$env:SystemRoot\Temp\$SrvSettingsXMLFile" -Force
 	}
 	try{
 		do {
 			Logit "Attempting download of XML settingsfile from https://raw.github.com/Hipster74/NewLabVM/master/NewLabVM/NewLabVM/$SrvSettingsXMLFile"
-			sleep 3      
-		} until(Invoke-WebRequest -Uri "https://raw.github.com/Hipster74/NewLabVM/master/NewLabVM/NewLabVM/$SrvSettingsXMLFile" -OutFile "$env:SystemRoot\Temp\$SrvSettingsXMLFile" -PassThru | Where-Object {$_.StatusCode -eq '200'})
+			sleep 3     
+		} until(Invoke-WebRequest -Uri "https://raw.github.com/Hipster74/NewLabVM/master/NewLabVM/NewLabVM/$SrvSettingsXMLFile" -UseBasicParsing -OutFile "$env:SystemRoot\Temp\$SrvSettingsXMLFile" -ErrorAction SilentlyContinue -PassThru | Where-Object {$_.StatusCode -eq '200'})
 		Logit "XMLFile downloaded from github"
 	}
 	Catch {
 		$ErrorMessage = $_.Exception.Message
 		Logit "Failed to download .xml from github: $ErrorMessage"
-		Write-Error $ErrorMessage
-		Break
+		Write-Error "Failed to download .xml from github with error $ErrorMessage"
+		Throw "Failed to download .xml from github"
 	}
-	sleep -Seconds 2
+	sleep -Seconds 20
 	[xml]$AdSrvSettings = Get-Content "$env:SystemRoot\Temp\$SrvSettingsXMLFile"
 	Logit "Getting settings from XMLFile into variables"
 
@@ -105,20 +106,27 @@ Inlinescript{
 	
 	$Section = "Configure Active Directory"
 	# Configure Active Directory and DNS
-	Logit "Configura AD DS and DNS"
-	Install-ADDSForest `
-	-CreateDnsDelegation:$false `
-	-DatabasePath "$ADDatabasePath" `
-	-DomainMode "$ADDomainMode" `
-	-DomainName $ADDomainName `
-	-DomainNetbiosName $DomainNetBios `
-	-ForestMode $ADForestMode `
-	-InstallDns:$true `
-	-SafeModeAdministratorPassword $ADSafeModeAdministratorPasswordSecure `
-	-LogPath "$ADLogPath" `
-	-NoRebootOnCompletion:$true `
-	-SysvolPath "$ADSysvolPath" `
-	-Force:$true
+	Logit "Configure AD DS and DNS"
+	try {
+		Install-ADDSForest `
+		-CreateDnsDelegation:$false `
+		-DatabasePath "$ADDatabasePath" `
+		-DomainMode "$ADDomainMode" `
+		-DomainName $ADDomainName `
+		-DomainNetbiosName $DomainNetBios `
+		-ForestMode $ADForestMode `
+		-InstallDns:$true `
+		-SafeModeAdministratorPassword $ADSafeModeAdministratorPasswordSecure `
+		-LogPath "$ADLogPath" `
+		-NoRebootOnCompletion:$true `
+		-SysvolPath "$ADSysvolPath" `
+		-Force:$true
+	} catch {
+		$ErrorMessage = $_.Exception.Message
+		Logit "Failed to run Install-ADDSForest: $ErrorMessage"
+		Write-Error $ErrorMessage
+		Break
+	}
 
 } -PSComputerName $VMName -PSCredential $VMCredential
 
@@ -126,7 +134,7 @@ Write-Verbose "Restarting computer $VMName to complete Active Directory installa
 Restart-Computer -PSComputerName $VMName -PSCredential $VMCredential -Wait -For WinRM -Force
 
 Inlinescript {
-
+	$SrvSettingsXMLFile = $using:SrvSettingsXMLFile
 	Function Logit{
 		$TextBlock1 = $args[0]
 		$TextBlock2 = $args[1]
@@ -138,7 +146,7 @@ Inlinescript {
 	Logit ("Successfully remoted to " + $env:COMPUTERNAME)
 	
 	$Section = "Get XMLFile"
-	[xml]$AdSrvSettings = Get-Content "$env:SystemRoot\Temp\ad_srv_settings.xml"
+	[xml]$AdSrvSettings = Get-Content "$env:SystemRoot\Temp\$SrvSettingsXMLFile"
 	Logit "Getting settings from XMLFile into variables"
 
 	# Settings DHCPServerDefaults
@@ -152,39 +160,46 @@ Inlinescript {
 	
 	$Section = "Configure DHCP Server"
 	# Authorize the DHCP Server in Active Directory
-	Logit "Authorize the DHCP Server in Active Directory"
-	Add-DhcpServerInDC -Verbose
+	try {
+		Logit "Authorize the DHCP Server in Active Directory"
+		Add-DhcpServerInDC -Verbose
 
-	# Add Scope to DHCP Server
-	Logit "Add Scope to DHCP Server"
-	Add-DhcpServerv4Scope `
-	-Name $DHCPScopeName `
-	-StartRange $DHCPScopeStart `
-	-EndRange $DHCPScopeEnd `
-	-SubnetMask $DHCPScopeSubnetMask `
-	-Verbose
+		# Add Scope to DHCP Server
+		Logit "Add Scope to DHCP Server"
+		Add-DhcpServerv4Scope `
+		-Name $DHCPScopeName `
+		-StartRange $DHCPScopeStart `
+		-EndRange $DHCPScopeEnd `
+		-SubnetMask $DHCPScopeSubnetMask `
+		-Verbose
 
-	# Set Options on scope
-	Logit "Set Options on scope"
-	$ScopeID = Get-DhcpServerv4Scope | Where-Object -Property Name -Like -Value "$DHCPScopeName"
-	Set-DhcpServerv4OptionValue `
-	-ScopeId $ScopeID.ScopeId `
-	-DnsDomain $DHCPScopeFQDN `
-	-DnsServer $DHCPScopeDNS `
-	-Router $DHCPScopeRouter `
-	-Verbose
+		# Set Options on scope
+		Logit "Set Options on scope"
+		$ScopeID = Get-DhcpServerv4Scope | Where-Object -Property Name -Like -Value "$DHCPScopeName"
+		Set-DhcpServerv4OptionValue `
+		-ScopeId $ScopeID.ScopeId `
+		-DnsDomain $DHCPScopeFQDN `
+		-DnsServer $DHCPScopeDNS `
+		-Router $DHCPScopeRouter `
+		-Verbose
 
-	# Add Security Groups
-	Logit "Add Security Groups"
-	Add-DhcpServerSecurityGroup -Verbose
+		# Add Security Groups
+		Logit "Add Security Groups"
+		Add-DhcpServerSecurityGroup -Verbose
 
-	# Flag DHCP as configured
-	Logit "Flag DHCP as configured in registry"
-	Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager\Roles\12 -Name ConfigurationState -Value 2 -Force -Verbose
+		# Flag DHCP as configured
+		Logit "Flag DHCP as configured in registry"
+		Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager\Roles\12 -Name ConfigurationState -Value 2 -Force -Verbose
 
-	# Restart DHCP Server
-	Logit "Restart DHCP Server"
-	Restart-Service "DHCP Server" -Force -Verbose
+		# Restart DHCP Server
+		Logit "Restart DHCP Server"
+		Restart-Service "DHCP Server" -Force -Verbose
+	} catch {
+		$ErrorMessage = $_.Exception.Message
+		Logit "Failed when configuring DHCP: $ErrorMessage"
+		Write-Error $ErrorMessage
+		Break
+	}
 
 } -PSComputerName $VMName -PSCredential $VMCredential
 	Write-Verbose "Done"
