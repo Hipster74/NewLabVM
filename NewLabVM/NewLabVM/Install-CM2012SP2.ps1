439222668415c127d1b7f82cba22c1cbc13433b7
Jeremias Jansson <meklubba52@hotmail.com>
2015-11-19 15:33:52 +0100

    Added MDT integration with CM

--- NewLabVM/NewLabVM/Install-CM2012SP2.ps1
+++ NewLabVM/NewLabVM/Install-CM2012SP2.ps1
@@ -13,8 +13,7 @@ workflow Install-CM2012SP2
     )    
     
     $ErrorActionPreference = "stop"
-
-    $CM2012SP2Unattend = Get-AutomationVariable $CM2012SP2UnattendName
+	$CM2012SP2Unattend = Get-AutomationVariable $CM2012SP2UnattendName
     
     inlinescript {
         $SourceFilesParentDir = $using:SourceFilesParentDir
@@ -24,7 +23,7 @@ workflow Install-CM2012SP2
             Write-Verbose "Starting CM 2012 With SP2 installation"
             if (Test-Path "$SourceFilesParentDir\SystemCenter\ConfigMgr2012wSP2\SMSSETUP\BIN\X64\setup.exe") {
                 # Save CM 2012 With SP2 unattendedconfiguration from SMA Asset to answerfile
-                $CM2012SP2Unattend | Out-File "$SourceFilesParentDir\SystemCenter\ConfigMgr2012wSP2\ConfigMgr2012Unattend.ini"
+                $CM2012SP2Unattend | Out-File "$SourceFilesParentDir\SystemCenter\ConfigMgr2012wSP2\ConfigMgr2012Unattend.ini" -Encoding utf8
                 # Save CM 2012 With SP2 commandline arguments as array
                 $CM2012SP2UnattendArg = @("/Script","$([char]34)$SourceFilesParentDir\SystemCenter\ConfigMgr2012wSP2\ConfigMgr2012Unattend.ini$([char]34)","/NoUserInput")
                 # Call CM 2012 With SP2 Setup.exe with arguments for unattended installation
@@ -62,6 +61,56 @@ workflow Install-CM2012SP2
             Write-Verbose "Failed to install Configuration manager 2012 With SP2"
             Write-Error $_.Exception
         }
+		# Integrate MDT with Configuration Manager
+		# Get Configuration Manager sitecode from WMI
+		$CMSiteCode = (Get-WmiObject -ComputerName $env:COMPUTERNAME -Namespace "root\SMS" -Class "SMS_ProviderLocation").SiteCode
+
+		# Get Configuration Manager 2012 Console installation path from registry
+		$CM12Path = (Get-ItemProperty "HKLM:\Software\Microsoft\ConfigMgr10\Setup" -Name "UI Installation Directory" -ErrorAction SilentlyContinue).'UI Installation Directory'
+
+		if ($CM12Path -ne $null) {
+			Write-Verbose "Found Configuration Manager 2012 Console installation at $CM12Path"
+
+		} else {
+			$CM12Path = (Get-ItemProperty "HKLM:\Software\wow6432node\Microsoft\ConfigMgr10\Setup" -Name "UI Installation Directory" -ErrorAction SilentlyContinue).'UI Installation Directory'
+    
+			if ($CM12Path -ne $null){
+				Write-Verbose "Found Configuration Manager 2012 Console(32-bit) installation at $CM12Path"
+    
+			} else {
+				Write-Error "Unable to locate Configuration Manager 2012 Console installation path from registry, aborting MDT integration"
+				Throw "Unable to locate Configuration Manager 2012 Console installation path from registry"  
+			}
+		}
+
+		# Get MDT Installation path from registry
+		$MDTPath = (Get-ItemProperty "HKLM:\Software\Microsoft\Deployment 4" -name Install_Dir -ErrorAction SilentlyContinue).Install_Dir
+		if ($MDTPath -ne $null) {
+			Write-Verbose "Found MDT installation path $MDTPath"
+
+		} else {
+			Write-Error "Unable to locate MDT installation path from registry, aborting MDT integration"
+			Throw "Unable to locate MDT installation path from registry"  
+		}
+
+		# Integrating MDT into the ConfigMgr 2012 console
+		Write-Verbose "Integrating MDT into the Configuration Manager 2012 console"
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.CM12Actions.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.CM12Actions.dll" -Force
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.Workbench.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.Workbench.dll" -Force
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.ConfigManager.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.ConfigManager.dll" -Force
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.CM12Wizards.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.CM12Wizards.dll" -Force
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.PSSnapIn.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.PSSnapIn.dll" -Force
+		Copy-Item -Path "$MDTPath\Bin\Microsoft.BDD.Core.dll" -Destination "$CM12Path\Bin\Microsoft.BDD.Core.dll" -Force
+		Copy-Item -Path "$MDTPath\Templates\CM12Extensions" -Destination "$CM12Path\XmlStorage\Extensions" -Force -Recurse
+
+		# Edit MOFFile with settings for our CMSite
+		Write-Verbose "Edit MOFFile with settings for our CMSite"
+		(Get-Content "$MDTPath\SCCM\Microsoft.BDD.CM12Actions.mof").replace('%SMSSERVER%', "$env:COMPUTERNAME") | Set-Content "$env:SystemRoot\temp\Microsoft.BDD.CM12Actions.mof"
+		(Get-Content "$env:SystemRoot\temp\Microsoft.BDD.CM12Actions.mof").replace('%SMSSITECODE%', "$CMSiteCode") | Set-Content "$env:SystemRoot\temp\Microsoft.BDD.CM12Actions.mof"
+
+		# Running mofcomp to compile new MOFFile, this must be done to be able to create MDT Task sequences etc. in CM console
+		Write-Verbose "Compiling MOFFile $env:SystemRoot\temp\Microsoft.BDD.CM12Actions.mof with MOFComp.exe"
+		& mofcomp.exe "$env:SystemRoot\temp\Microsoft.BDD.CM12Actions.mof"
                            
     } -PSComputerName $VMName -PSCredential $VMCredential -PSAuthentication CredSSP # CredSSP required for Configuration Manger Setup to be able to verify Active Directory connection
 }
